3ad5ae2dc3a101fa6cf7d3d5900e7dd75a5dab3b
liquid0624 <rjbirnesser@gmail.com>
2017-09-16 01:36:53 -0400

    cheeseburger: remove assert and board info
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,8 +29,8 @@
 DEVICE_PATH := device/oneplus/cheeseburger
 
 # Assertions
-TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
-TARGET_OTA_ASSERT_DEVICE := cheeseburger
+#TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
+#TARGET_OTA_ASSERT_DEVICE := cheeseburger
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
